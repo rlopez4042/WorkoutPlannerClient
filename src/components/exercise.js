99a86978a8bb3f9a349aa263ca47cd6dab59e00a
isaiah <isaiah.keller@gmail.com>
2022-03-08 11:13:49 -0800

    added framework to components

---
+++ src/components/exercise.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Exercise() {
+
+    return (
+        <div className='exercise'>
+            
+        </div>
+    );
+}
+
+export default Exercise;
