d9920e5ff5e4daf80bad56bf331f54cafe90b066
Garrett <garrettpyke@msn.com>
2022-03-08 20:45:44 -0700

    added ability to POST new workout, but not with Exercises yet

---
+++ src/components/exerciseForm.js
@@ -0,0 +1,20 @@
+//import React from 'react'
+import { useState } from 'react'
+
+export default function exerciseForm() {
+
+    
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input />
+                <input />
+                <input />
+                <input />
+            </form>
+        </>
+    )
+
+
+}
