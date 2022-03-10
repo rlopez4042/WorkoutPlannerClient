7967359e15568256182fa0598a27dc9a4f882b9e
Garrett <garrettpyke@msn.com>
2022-03-10 14:31:38 -0700

    change from hard-coded Add Exercise to pass in Exercise ID by params, not functional yet

--- src/components/ExerciseDetail.js
+++ src/components/ExerciseDetail.js
@@ -6,12 +6,12 @@ function ExerciseDetail({workout}) {
     if(workout) {
         exerciseList = workout.exercises.map((exercise, index) => {
         return (
-            <>
+            <div key={index}>
                 <li>{exercise.name}</li>
                 <li>{exercise.description}</li>
                 <li>{exercise.reps}</li>
                 <li>{exercise.sets}</li>
-            </>
+            </div>
         )})
         console.log("exerciseList: ", exerciseList) //returns one 'undefined' for every item in array (number of objects in exerciseList is equal to number of items in workout.exercises array, but they all show 'undefined' in console and therefore don't render )
         return (
--- src/components/form.js
+++ src/components/form.js
@@ -6,6 +6,8 @@ function Form() {
 
   const [workouts, setWorkouts] = useState([])
   const [workout, setWorkout] = useState({})
+  const [toggleDisplay, setToggleDisplay] = useState(true)
+  const [workoutToEdit, setWorkoutToEdit] = useState("")
 
     const handleClick = () => {
       fetch("http://localhost:4000/workout")
@@ -16,7 +18,7 @@ function Form() {
 
     const handleChange = (event) => {
         event.persist()
-        console.log("workout: ", workout)
+        //console.log("workout: ", workout)
         setWorkout(prevWorkout =>{
           const editedWorkout = {...prevWorkout, [event.target.name]:
           event.target.value}
@@ -26,7 +28,7 @@ function Form() {
       
       const handleSubmit = event => {
         event.preventDefault()
-        console.log("workout: ", workout)
+        //console.log("workout: ", workout)
         fetch("http://localhost:4000/workout", {
           headers: {
             'Content-Type': 'application/json'
@@ -40,44 +42,43 @@ function Form() {
           .then(() => setWorkout({name: "", duration: "", exercises:[{name:"", description:"", sets:"", reps:""}]}))
       }
 
-      function getWorkout(workoutId) {
+    function editWorkout(event) {
+      //console.log("event: ", event.target.value)
+      setWorkoutToEdit(event.target.value)
+    }  
 
-      }
+    console.log("workoutToEdit: ", workoutToEdit)
 
-    const workoutList = workouts.map((workout, index) => {
-      return (
-        <div key={index}>
-          <h4>{workout.name}</h4>
-          <p>{workout.duration}</p>
-          <button>Edit Workout</button>
-          {workout.exercises.map((exercise, i) => {
-            return (
-            <div key={i}>
-              <h5>{exercise.name}</h5>
-              <p>{exercise.description}</p>
-              <ul>
-                <li>{exercise.reps}</li>
-                <li>{exercise.sets}</li>
-              </ul>
+      const workoutList = workouts.map((workout, index) => {
+        return (
+          <div key={index}>
+            <h4>{workout.name}</h4>
+            <p>{workout.duration}</p>
+            <button onClick={editWorkout} value={workout._id}>Edit Workout</button>
+            {workout.exercises.map((exercise, i) => {
+              return (
+              <div key={i}>
+                <h5>{exercise.name}</h5>
+                <ul>
+                  <li>{exercise.description}</li>
+                  <li>{exercise.reps}</li>
+                  <li>{exercise.sets}</li>
+                </ul>
+              </div>
+            )})}
             </div>
-          )})}
-          </div>
-      )
-    })
+        )
+      }) 
 
     return (
         <>
     <form onSubmit={handleSubmit}>
         <input onChange={handleChange} value={workout.name} name="name" placeholder="Workout Name"/>
         <input onChange={handleChange} value={workout.duration} name="duration" placeholder="Estimated Time"/>
-        {/* <input onChange={handleChange} value={workout.exercises.name} name="Exercise Name" placeholder="Exercise Name"/> */}
-        {/* <input onChange={handleChange} value={workout.exercises.description} name="Exercise Description" placeholder="Exercise Description"/> */}
-        {/* <input onChange={handleChange} value={workout.exercises.sets} name="Number of sets" placeholder="Number of sets"/> */}
-        {/* <input onChange={handleChange} value={workout.exercises.reps} name="Number of reps" placeholder="Number of reps"/> */}
-
         <button type="Submit">Add Workout</button>
       </form>
-      <Exercise />
+      {/* { toggleDisplay ? ( <Exercise /> ) : null  } */}
+      <Exercise workoutId={workoutToEdit}/>
       <button onClick={handleClick}>View Workouts</button> 
       {workoutList}
     </>
