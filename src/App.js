72bbe7c896cd051eb22a460cd4029365a855680b
Robert Lopez <robertlopez@Roberts-MBP.lan>
2022-03-17 13:47:35 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import Form from './components/form'
-import { Route, Link, Routes, Navigate } from 'react-router-dom';
-import Workout from './components/workout'
-import Exercise from './components/exercise';
-
-
+import React, { useState, useEffect } from "react";
+import Form from "./components/form";
+import { Route, Link, Routes, Navigate } from "react-router-dom";
+import Workout from "./components/workout";
+import Exercise from "./components/exercise";
 
 function App() {
-
   return (
-    <div className='App card-container'>
-      {/* <main> */}
-      <Routes>
-        {/* <Route path='/' element={<Workout/>} /> */}
-        {/* <Route path="/exercise" element={<Exercise />} /> */}
-
-      </Routes>
-      < Form />
-      {/* </main> */}
+    <div className="body">
+      <div className="App card-container">
+        {/* <main> */}
+        <Routes>
+          {/* <Route path='/' element={<Workout/>} /> */}
+          {/* <Route path="/exercise" element={<Exercise />} /> */}
+        </Routes>
+        <Form />
+        {/* </main> */}
+      </div>
     </div>
-  )
-
-
+  );
 }
 
 export default App;
